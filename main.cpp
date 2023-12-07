3483cbeda05b12d0fd032509a705788610e4b0d2
Maxime Le Sage <mlesa084@uottawa.ca>
2023-12-07 14:36:13 -0500

    More changes to main

--- main.cpp
+++ main.cpp
@@ -6,51 +6,57 @@ int main() {
 
   string p1_name;
   string p2_name;
-  string winner_name;
 
   // Initialiser les composantes
   const int MAX_NUM_PLAYER = 2;
   Player *p1 = nullptr;
   Player *p2 = nullptr;
   Player **pArr = new Player *[MAX_NUM_PLAYER];
-  DiscardPile *dp = new DiscardPile;
+
   CardFactory *cf = CardFactory::getFactory();
   Deck *deck = nullptr;
+  DiscardPile *dp = new DiscardPile;
   TradeArea *trAr = new TradeArea;
+
   Table *tb;
 
-  char user_input[2];
+  char input[2];
 
   // Continuer une partie sauvegardée
   cout << "Voulez-vous continuer une partie sauvegardee? (o/n)" << endl;
-  cin >> user_input;
+  cin >> input;
 
   // Chercher les fichiers, sinon les initialiser
-  if (user_input[0] == 'o') {
+  if (input[0] == 'o') {
     tb = new Table(*p1, *p2, *dp, *trAr, *deck, *cf);
+
+    // Obtenir les joueurs
+    tb->reloadPlayer(1);
+    tb->reloadPlayer(2);
+    p1 = tb->getPlayer(1);
+    p2 = tb->getPlayer(2);
 
     // Obtenir le Deck
     tb->reloadDeck();
     deck = tb->getDeck();
 
-    tb->reloadPlayer(1); // Obtenir le joueur 1
-    tb->reloadPlayer(2); // Obtenir le joueur 2
-    p1 = tb->getPlayer(1);
-    p2 = tb->getPlayer(2);
-
-    tb->reloadDiscardPile(); // Obtenir la DiscardPile
+    // Obtenir la DiscardPile
+    tb->reloadDiscardPile();
     dp = tb->getDiscardPile();
 
-    tb->reloadTradeArea(); // Obtenir le TradeArea
+    // Obtenir le TradeArea
+    tb->reloadTradeArea();
     trAr = tb->getTradeArea();
 
   } else { // Initialiser une nouvelle partie
     cout << endl;
     cout << "Entrez le nom du premier joueur (sans espaces) : ";
     cin >> p1_name;
+
     cout << endl;
     cout << "Entrez le nom du deuxieme joueur (sans espaces) : ";
     cin >> p2_name;
+
     cout << endl;
 
     // Créer les joueurs
@@ -62,16 +68,16 @@ int main() {
     pArr[0] = p1;
     pArr[1] = p2;
 
-    deck = tb->getDeck();      // Obtenir le Deck
-    dp = tb->getDiscardPile(); // Obtenir la DiscardPile
-    trAr = tb->getTradeArea(); // Obtenir le TradeArea
-
-    cout << "Donner 5 cartes a chaque joueur " << endl;
+    //Obtenir les éléments restants
+    deck = tb->getDeck();
+    dp = tb->getDiscardPile();
+    trAr = tb->getTradeArea();
 
     // Piger 5 cartes par joueur
-    for (int player = 0; player < MAX_NUM_PLAYER; player++) {
-      for (int card = 0; card < 5; card++) {
-        pArr[player]->takeCard(deck->draw());
+    cout << "Donner 5 cartes a chaque joueur " << endl;
+    for (int joueur = 0; joueur < MAX_NUM_PLAYER; joueur++) {
+      for (int carte = 0; carte <= 4; carte++) {
+        pArr[joueur]->takeCard(deck->draw());
       }
     }
   }
@@ -80,8 +86,8 @@ int main() {
   while (deck->size() != 0) {
     cout << "Taille du paquet : " << deck->size() << endl;
     cout << endl << "Voulez-vous sauvegarder votre partie et arreter? (o/n)" << endl;
-    cin >> user_input;
-    if (user_input[0] == 'o') {
+    cin >> input;
+    if (input[0] == 'o') {
       // Sauvegarder le jeu
       tb->saveTable();
       cout << "Partie sauvegardee" << endl;
@@ -96,8 +102,7 @@ int main() {
         cout << endl;
         cout << "Tour du joueur " << i + 1 << endl;
         cout << endl;
-
-        Player *p = tb->getPlayer(i); // Obtenir le joueur courant
+        Player *p = tb->getPlayer(i);
 
         // Piger une carte
         cout << "Obtention de la premiere carte du paquet" << endl;
@@ -120,6 +125,7 @@ int main() {
                 cout << " ajoutee de l'espace d'echange a la chaine "
                      << chain->getChainType() << " du joueur" << endl;
                 *chain += trAr->trade(card->getName());
+                
                 cardAdded = true;
               }
             }
@@ -128,6 +134,7 @@ int main() {
               cout << "Carte ";
               card->print(cout);
               cout << " ajoutee de l'espace d'echange a la pile de discarte" << endl;
+
               *dp += trAr->trade(card->getName());
             }
           }
@@ -151,9 +158,9 @@ int main() {
              << "Voulez-vous jouer la premiere carte de votre main ("
              << p->getHand()->top()->getName()[0] << ")? (o/n) " << endl;
 
-        cin >> user_input;
+        cin >> input;
 
-        if (user_input[0] == 'o') {
+        if (input[0] == 'o') {
 
           cout << endl
                << *p << endl
@@ -173,40 +180,41 @@ int main() {
              << "Voulez-vous placer une carte dans la pile de discarte? (o/n) "
              << endl;
 
-        cin >> user_input;
+        cin >> input;
 
-        if (user_input[0] == 'o') {
+        if (input[0] == 'o') {
 
-          int idx;
           Card *card = nullptr;
+          int index;
 
           cout << endl << "Main du joueur " << i + 1 << endl;
           p->printHand(cout, true);
           cout << "Taille de la main: " << p->getNumCards() << endl;
           cout << "Entrez l'index de la carte que vous voulez retirer (0 a "
                << p->getNumCards() - 1 << "): ";
-          cin >> idx;
+          cin >> index;
 
           // Enlever la carte
-          card = p->removeCard(idx);
+          card = p->removeCard(index);
 
           // Valider l'entrée jusqu'à ce que la carte soit enlevée
           while (card == nullptr) {
             cout << endl
-                 << "La carte n'a pas pu etre retiree. Cet index est hors-limites: " << idx
+                 << "La carte n'a pas pu etre retiree. Cet index est hors-limites: " << index
                  << endl;
             cout << "Taille de la main: " << p->getNumCards() << endl;
             cout
                 << "Entrez l'index de la carte que vous voulez retirer (0 a "
                 << p->getNumCards() - 1 << "): ";
-            cin >> idx;
-            card = p->removeCard(idx);
+            cin >> index;
+            card = p->removeCard(index);
           }
 
           // Ajouter la carte à la DiscardPile
           cout << "Carte ";
           card->print(cout);
           cout << " ajoutee a la pile de discarte" << endl;
+
           *dp += card;
         }
 
@@ -214,7 +222,7 @@ int main() {
         cout << endl
              << "Piger trois cartes du paquet vers l'espace d'echange" << endl
              << endl;
-        for (int drw = 0; drw < 3; drw++) {
+        for (int i = 0; i < 3; i++) {
           *trAr += (deck->draw());
         }
 
@@ -223,6 +231,7 @@ int main() {
           cout << "Carte ";
           dp->top()->print(cout);
           cout << " ajoutee de la pile de discarte vers l'espace d'echange" << endl;
+
           *trAr += (dp->pickUp());
         }
 
@@ -239,19 +248,17 @@ int main() {
           cout << endl << "Voulez-vous chainer la carte ";
           card->print(cout);
           cout << "? (o/n) " << endl;
-          cin >> user_input;
+          cin >> input;
 
-          if (user_input[0] == 'o') {
+          if (input[0] == 'o') {
             p->playCard(trAr->trade(card->getName()), true);
-
             cout << endl << *p << endl;
 
             cout << endl << "Main du joueur " << i + 1 << endl;
             p->printHand(cout, true);
+
             cout << endl << "Espace d'echange: " << *trAr << endl;
-
           } else {
-
             cout << endl << "Carte laissee dans l'espace d'echange" << endl;
           }
         }
@@ -264,11 +271,11 @@ int main() {
           p->takeCard(deck->draw());
         }
 
+        // Afficher l'info à la fin du tour
         cout << endl << "Main du joueur " << i + 1 << endl;
         p->printHand(cout, true);
-        cout << endl;
 
-        cout << endl << "Pile de discarte: ";
+        cout << endl << endl << "Pile de discarte: ";
         dp->print(cout);
         cout << endl;
       }
@@ -279,10 +286,13 @@ int main() {
   if (deck->size() == 0) {
     cout << endl << "Info de la table" << endl << endl << *tb << endl;
     cout << "Aucune carte restante dans le paquet" << endl;
-    tb->win(winner_name);
-    cout << endl << "Le gagnant est: " << winner_name << endl;
+
+    string gagnant;
+    tb->win(gagnant);
+    cout << endl << "Le gagnant est: " << gagnant << endl;
   }
 
+  // Libérer la mémoire
   delete tb;
   delete[] pArr;
 
